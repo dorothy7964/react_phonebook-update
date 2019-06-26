f5abbc5237d9b521bd46c7234c137e11632960f8
SEC\user <dorothy_007@naver.com>
2019-06-26 20:49:02 +0900

    버튼 Toggle 구현

--- src/component/ConteactDetails.js
+++ src/component/ConteactDetails.js
@@ -9,14 +9,25 @@ class ConteactDetails extends Component {
     onRemove: () => console.warn('onRemove not defined'),
   }
 
+  state = {
+    isEdit : false,
+  }
+
+  handleToggle = () => {
+    this.setState({
+      isEdit : !this.state.isEdit
+    });
+  }
+
   render() {
+    const { isEdit } = this.state;
     const { contact, onRemove } = this.props;
     return(
       <div>
         <h1>ConteactDetails</h1>
         <div>{contact.name}</div>
         <div>{contact.phone}</div>
-        <button>Edit</button>
+        <button onClick={this.handleToggle}>{isEdit? 'OK':'Edit'}</button>
         <button onClick={onRemove}>Remove</button>
       </div>
     );
