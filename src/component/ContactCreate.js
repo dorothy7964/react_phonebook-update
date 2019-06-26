3d52fa6da7a6da0b6dc13fbd374466dcad534b99
SEC\user <dorothy_007@naver.com>
2019-06-26 20:02:26 +0900

    ContactCreate 여러 input 데이터 가져오기
    객체 생성 후 [e.target.name] = e.target.value;

--- src/component/ContactCreate.js
+++ src/component/ContactCreate.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
 class ContactCreate extends Component {
+  state = {
+    name : '',
+    phone : '',
+  }
+
+  handleChange = (e) => {
+    const nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
   render() {
+    const { name, phone } = this.state;
     return(
       <div>
         <h1>ContactCreate</h1>
@@ -10,11 +22,15 @@ class ContactCreate extends Component {
             type="text"
             name="name"
             placeholder="name"
+            value={name}
+            onChange={this.handleChange}
           />
           <input
             type="text"
             name="phone"
             placeholder="phone"
+            value={phone}
+            onChange={this.handleChange}
           />
         </p>
         <button>Create</button>
